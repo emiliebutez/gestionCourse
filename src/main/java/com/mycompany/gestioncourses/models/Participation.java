b137003e83d96a12d82a1d7d74b0b7dd3a6a35c9
emiliebutez <emiliebutez@live.fr>
2022-10-18 22:58:23 +0200

    feat: service participation complété

--- src/main/java/com/mycompany/gestioncourses/models/Participation.java
+++ src/main/java/com/mycompany/gestioncourses/models/Participation.java
@@ -17,20 +17,17 @@ public class Participation extends Model {
     @GeneratedValue
     private Integer numInscription;
     private Etat etatParticipation;
-    private String nomEquipe;
     @OneToMany
     private List<Performance> performances;
     @ManyToOne
     private Edition edition;
     @ManyToOne
     private Coureur coureur;
+    @ManyToOne
+    private Equipe equipe;
 
     public void annulerParticipation() {
         this.etatParticipation = Etat.Annulee;
-    }
-
-    public void modifierInscription(String nomEquipe) {
-        this.nomEquipe = nomEquipe;
     }
 
     public void eliminerParticipant() {
--- src/main/java/com/mycompany/gestioncourses/services/ParticipationService.java
+++ src/main/java/com/mycompany/gestioncourses/services/ParticipationService.java
@@ -1,5 +1,9 @@
 package com.mycompany.gestioncourses.services;
 
+import com.mycompany.gestioncourses.models.*;
+
+import java.util.Collections;
+
 public class ParticipationService {
     private static ParticipationService INSTANCE;
 
@@ -14,4 +18,52 @@ public class ParticipationService {
     private ParticipationService() {
 
     }
+
+    public Participation creerParticipation(Edition edition, Coureur coureur, Equipe equipe) {
+        Participation participation = new Participation(
+                null,
+                Etat.Validee,
+                Collections.emptyList(),
+                edition,
+                coureur,
+                equipe
+        );
+
+        participation.save();
+        edition.refresh();
+        coureur.refresh();
+        equipe.refresh();
+
+        return participation;
+    }
+
+    public Participation changerEquipeParticipation(Participation participation, Equipe equipe) {
+        Equipe ancienne = participation.getEquipe();
+
+        participation.setEquipe(equipe);
+        participation.save();
+
+        ancienne.refresh();
+        equipe.refresh();
+
+        return participation;
+    }
+
+    public Participation eliminerParticipant(Participation participation) {
+        participation.setEtatParticipation(Etat.Eliminee);
+        participation.save();
+        participation.getCoureur().refresh();
+
+        return participation;
+    }
+
+    public Participation annulerParticipation(Participation participation) {
+        participation.setEtatParticipation(Etat.Annulee);
+        participation.save();
+        participation.getCoureur().refresh();
+        participation.getEdition().refresh();
+
+        return participation;
+    }
+
 }
