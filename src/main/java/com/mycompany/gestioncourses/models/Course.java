86c554b1fb58087025725dca74ebcfaa6e9ba1ea
DavidCh97 <58686481+DavidCh97@users.noreply.github.com>
2022-10-18 23:44:15 +0200

    V1-methodes Course

--- src/main/java/com/mycompany/gestioncourses/models/Course.java
+++ src/main/java/com/mycompany/gestioncourses/models/Course.java
@@ -1,6 +1,10 @@
 package com.mycompany.gestioncourses.models;
 
 import io.ebean.Model;
+import java.io.Serializable;
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.Iterator;
 import lombok.*;
 
 import javax.persistence.*;
@@ -13,11 +17,58 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-public class Course extends Model {
+public class Course extends Model implements Serializable {
     @Id
     @GeneratedValue
     private Integer id;
     private String nom;
     @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-    private List<Edition> editions;
+    private ArrayList<Edition>editions;
+    
+    public String afficherNom(){
+        return this.nom;
+    }
+
+    public Course(Integer id, String nom) {
+        this.id = id;
+        this.nom = nom;
+        this.editions = new ArrayList<Edition>();
+    }
+    
+    public void afficherEdition(Edition edition){
+       for(int i=0;i<this.editions.size();i++){
+        if(this.editions.get(i) == edition){
+            System.out.println("Année : "+edition.getAnnee());
+            System.out.println("Date de début : "+edition.getDateDebut());
+            System.out.println("Date de fin : "+edition.getDateFin());
+        }
+      }
+    }
+    
+    public void afficherListEdition(){
+        for(Edition e : editions){
+            System.out.println(e.getId()+"\t"+e.getAnnee()+"\t"+e.getDateDebut()+"\t"+e.getDateFin()+"\t"+e.getPaysDepart()+"\t"+e.getPaysArrivee()+"\t"+e.getDistance());
+        }
+    }
+    
+    public void ajouterEdition(Edition edition){
+        this.editions.add(edition);
+    }
+    
+    public void rechercherCourse(String nomCourse){
+        //this.editions.contains(nomCourse);
+    }
+    
+    public ArrayList<Edition>recupererListesEditions(){
+         Iterator iter = editions.iterator();
+         ArrayList<Edition>list = new ArrayList<>();
+        while (iter.hasNext()){
+            list.add((Edition) iter.next());
+        }
+        return list;
+    }
+    
+    
+    
+   
 }
