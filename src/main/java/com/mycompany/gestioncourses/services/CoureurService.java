c808c856d7cbbee7a9b29b077198d4a8ab6c0f53
emiliebutez <emiliebutez@live.fr>
2022-10-18 23:01:29 +0200

    feat: service coureur complété

--- src/main/java/com/mycompany/gestioncourses/services/CoureurService.java
+++ src/main/java/com/mycompany/gestioncourses/services/CoureurService.java
@@ -1,5 +1,13 @@
 package com.mycompany.gestioncourses.services;
 
+import com.mycompany.gestioncourses.models.Coureur;
+import com.mycompany.gestioncourses.models.query.QCoureur;
+
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
 public class CoureurService {
     private static CoureurService INSTANCE;
 
@@ -13,6 +21,19 @@ public class CoureurService {
 
     private CoureurService() {
 
+    }
+
+    public Coureur creerCoureur(String nom, String prenom, Date dateNaissance, String groupeSanguin) {
+        Coureur coureur = new Coureur(null, nom, prenom, dateNaissance, groupeSanguin, Collections.emptyList());
+        coureur.save();
+        return coureur;
+    }
+
+    public List<Coureur> rechercherCoureur(String nom, String prenom) {
+        return new QCoureur()
+                .nom.icontains(nom)
+                .prenom.icontains(prenom)
+                .findList();
     }
 
 }
