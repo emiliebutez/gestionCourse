a82ba0fbf09e67dbd33e052715b05646f5449e9a
emiliebutez <emiliebutez@live.fr>
2022-10-18 23:50:36 +0200

    feat: Complété PerformanceService

--- src/main/java/com/mycompany/gestioncourses/services/PerformanceService.java
+++ src/main/java/com/mycompany/gestioncourses/services/PerformanceService.java
@@ -1,5 +1,10 @@
 package com.mycompany.gestioncourses.services;
 
+import com.mycompany.gestioncourses.models.Etape;
+import com.mycompany.gestioncourses.models.Participation;
+import com.mycompany.gestioncourses.models.Performance;
+import jdk.internal.perf.Perf;
+
 public class PerformanceService {
     private static PerformanceService INSTANCE;
 
@@ -14,4 +19,35 @@ public class PerformanceService {
     private PerformanceService() {
 
     }
+
+    public Performance creerPerformance(Participation participation, Etape etape, float temps, int pointsSprint, int pointGrimp) {
+        Performance performance = new Performance(null, temps, pointsSprint, pointGrimp, participation, etape);
+        performance.save();
+        participation.refresh();
+        etape.refresh();
+
+        return performance;
+    }
+
+    public Performance ajouterPointsSprint(Performance performance, int pointsSprint) {
+        performance.setPointsSprint(performance.getPointsSprint() + pointsSprint);
+        performance.save();
+
+        return performance;
+    }
+
+    public Performance ajouterPointsGrimp(Performance performance, int pointsGrimp) {
+        performance.setPointsGrimp(performance.getPointsGrimp() + pointsGrimp);
+        performance.save();
+
+        return performance;
+    }
+
+    public Performance ajouterTemps(Performance performance, float delta) {
+        performance.setTemps(performance.getTemps() + delta);
+        performance.save();
+
+        return performance;
+    }
+
 }
