44355986b4a86f6d7acde67fe094a1f6b891a871
DavidCh97 <58686481+DavidCh97@users.noreply.github.com>
2022-10-18 23:44:34 +0200

    V1-methodes edition

--- src/main/java/com/mycompany/gestioncourses/models/Edition.java
+++ src/main/java/com/mycompany/gestioncourses/models/Edition.java
@@ -1,10 +1,15 @@
 package com.mycompany.gestioncourses.models;
 
 import io.ebean.Model;
+import java.util.AbstractList;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import lombok.*;
 
 import javax.persistence.*;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 
 @Table
@@ -30,23 +35,113 @@ public class Edition extends Model {
     private boolean annulee;
 
     @OneToMany
-    private List<Etape> etapes;
+    private ArrayList<Etape>etapes;
 
     @OneToMany
-    private List<Participation> participations;
+    private ArrayList<Participation> participations;
 
     @OneToMany
-    private List<ParticipationEquipe> participationEquipes;
+    private ArrayList<ParticipationEquipe> participationEquipes;
 
     @ManyToOne
     private Course course;
 
+    public Edition(Integer id, int annee, Date dateDebut, Date dateFin, String villeDepart, String villeArrivee, String paysDepart, String paysArrivee, float distance, boolean annulee){
+        this.id = id;
+        this.annee = annee;
+        this.dateDebut = dateDebut;
+        this.dateFin = dateFin;
+        this.villeDepart = villeDepart;
+        this.villeArrivee = villeArrivee;
+        this.paysDepart = paysDepart;
+        this.paysArrivee = paysArrivee;
+        this.distance = distance;
+        this.annulee = annulee;
+        this.etapes = new ArrayList<Etape>();
+        this.participations = new ArrayList<Participation>();
+        this.participationEquipes = new ArrayList<ParticipationEquipe>();
+        this.course = course;
+    }
+    
+   
     public void ajouterEtape(Etape etape) {
         this.etapes.add(etape);
     }
 
-    public List<Equipe> classementEquipe() {
-        throw new IllegalStateException("classement équipe non implémenté");
+//    public ArrayList<Equipe>classementEquipe() {
+//       return etapes;
+//    }
+//    
+//    public ArrayList<Equipe>classementGeneral() {
+//       return 0;
+//    }
+    
+    public void afficherCoureurs(){
+          Iterator iter = participations.iterator();
+        while (iter.hasNext()){
+            System.out.println(iter.next());
+        }
+    }
+    
+    public void afficherEquipes(){
+        Iterator iter = participationEquipes.iterator();
+        while (iter.hasNext()){
+            System.out.println(iter.next());
+        }
+    }
+    
+    public void afficherEtapes(){
+       Iterator iter = etapes.iterator();
+        while (iter.hasNext()){
+            System.out.println(iter.next());
+        } 
+    }
+    
+    public void modifierInformations(int annee, Date dateDebut , Date dateFin, String villeDepart, String villeArrivee, String paysArrivee, String paysDepart){
+        this.annee = annee;
+        this.dateDebut = dateDebut;
+        this.dateFin = dateFin;
+        this.villeDepart = villeDepart;
+        this.villeArrivee = villeArrivee;
+        this.paysArrivee = paysArrivee;
+        this.paysDepart = paysDepart;   
+    }
+    
+    public boolean annulerEdition(){
+        return this.annulee = true;
+    }
+    
+    public void afficherInformations(){
+        System.out.println("Année :"+this.annee);
+        System.out.println("Ville de départ: "+this.villeDepart);
+        System.out.println("Ville d'arrivée: "+this.villeArrivee);
+        System.out.println("Date de début: "+this.dateDebut);
+        System.out.println("Date de fin : "+this.dateFin);
+        System.out.println("Pays de départ : "+this.paysDepart);
+        System.out.println("Pays d'arrivée : "+this.paysArrivee);        
+    }
+    
+//    public Coureur recupererMeilleurSprinteur(){
+//        
+//    }
+//    
+//    public Coureur recupererMeilleurGrimpeur(){
+//        
+//    }
+//    
+//    public Coureur recupererMeilleurJeune(){
+//        
+//    }
+//    
+//    public float tempsMoyenCoureur(){
+//        
+//    }
+    
+    
+    
+    public void afficherPlusLongueCourse(){
+       
     }
 
+    
 }
